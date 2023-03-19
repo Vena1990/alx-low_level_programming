ac97c25c02845387ffb745fa3be4b24fdc373d1f
Vena1990 <vena.tshaka@gmail.com>
2023-03-19 08:54:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - main entry point
-*return : always 0 (success)
+*main- this is the main function
+*
+*Return: always 0
 */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle);
+return (0);
 }
