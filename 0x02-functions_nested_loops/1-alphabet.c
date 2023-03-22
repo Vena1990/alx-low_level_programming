c4356683716e82657eae7302e4daab83f804a36b
Vena1990 <vena.tshaka@gmail.com>
2023-03-22 13:23:35 -0700

    remove

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+
+/**
+*print_alphabet - Make the alphabet
+*Return: void
+*/
+
+void print_alphabet(void)
+
+{
+
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_pitchar(c);
+}
+_putchar('\n');
+}
