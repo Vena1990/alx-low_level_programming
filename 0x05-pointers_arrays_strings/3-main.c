7c8cfe51efe96501d061d7737b0e2bea07081325
Vena1990 <vena.tshaka@gmail.com>
2023-03-28 16:07:25 -0700

    3-main.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-    char *str;
+	char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
 }
 
