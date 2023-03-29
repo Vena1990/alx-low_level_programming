25057579aa13a5d513ff82cd8ae574821ab4296b
Vena1990 <vena.tshaka@gmail.com>
2023-03-28 17:05:43 -0700

    correction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,13 @@
 
 void print_rev(char *s)
 {
-int i, n;
-n = strlen(s);
-for (i = n - 1; i >= 0; i--)
-{
-_putchar(s[i]);
-}
+	int i, n;
+	n = o
+	while (s[n] != '\0')
+	n++;
+	for (i = n - 1; i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
 }
