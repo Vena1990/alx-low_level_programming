7dc1023e4baaec7c13d4684bc1a9d048ebdb3bc1
Vena1990 <vena.tshaka@gmail.com>
2023-03-30 11:31:26 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,28 @@
 #include "main.h"
 
 /**
-*_strcmp - compare string values
-*@s1: input value
-*@s2: input value
-*Return: s1[i] - s2[i]
+*_strncpy - copy a string
+*@dest: input value
+*@src: input value
+*@n: input value
+*Return: dest
 */
 
-int _strcmp(char *s1, char *s2)
+int _strncpy(char *dest, char *src, int n)
 {
 
-	int i;
+int j;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
-	{
-	if (s1[i] != s2[i])
-	{
-	return (s1[i] - s2[i]);
-	}
-		i++;
-	}
-	return (0);
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j]
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
 }
